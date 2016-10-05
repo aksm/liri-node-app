48934993ff853d6a9696e717412319e27c909c8f
aksm <albert.min@gmail.com>
2016-10-05 12:08:28 -0400

    Initial files

---
+++ liri.js
@@ -0,0 +1,5 @@
+var t = require("./keys.js");
+var tckey = t.twitterKeys.consumer_key;
+var tcsecret = t.twitterKeys.consumer_secret;
+var ttkey = t.twitterKeys.access_token_key;
+var ttsecret = t.twitterKeys.access_token_secret;
